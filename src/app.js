d6de2061d6c6bd7ed89287a89f63c47040aa6ead
bynx <138079781+sicr2000@users.noreply.github.com>
2023-08-09 00:09:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["el", "nuestro", "tu"];
+  let adj = ["gran", "enorme", "sabroso", "pequeño"];
+  let nombres = ["carro", "binks", "JavaScript", "racoon", "master"];
+  let extensiones = [".com", ".net", ".org", ".ve", ".es"];
+  let results = "";
+
+  pronombres.forEach(pronombre => {
+    adj.forEach(adjetivo => {
+      nombres.forEach(nombre => {
+        extensiones.forEach(extension => {
+          console.log(pronombre + adjetivo + nombre + extension);
+          results = results + "\n" + pronombre + adjetivo + nombre + extension;
+        });
+      });
+    });
+  });
+  let domainGenerator = document.getElementById("results");
+  domainGenerator.innerHTML = "<pre>" + results + "</pre>";
 };
